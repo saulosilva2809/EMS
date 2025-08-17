fbe192fcc469a8060608d8c92602d91b77b3a9cf
Saulo Emmanuel <saulocomercial7@gmail.com>
2025-08-17 00:57:35 -0300

    feat: implement password recovery API endpoint

--- backend/apps/authentication/api/v1/urls.py
+++ backend/apps/authentication/api/v1/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .views import SendCodeForRecoverPasswordAPI
+
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "send-code-for-recovery-password/",
+        SendCodeForRecoverPasswordAPI.as_view(),
+        name="send_code_for_recovery_password"
+    ),
 ]
--- backend/apps/authentication/api/v1/views.py
+++ backend/apps/authentication/api/v1/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.template.loader import render_to_string
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import SendCodeForRecoverPasswordSerializer
+from apps.authentication.service.generate_code import generate_code
+from apps.tasks.tasks import send_email
+
+
+class SendCodeForRecoverPasswordAPI(APIView):
+    def post(self, request):
+        serializer = SendCodeForRecoverPasswordSerializer(data=request.data)
+
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            code = generate_code()
+
+            try:
+                send_email.delay(
+                    subjetc='Código para Recuparação de Senha - EMS',
+                    message=None,
+                    recipient_list=[email],
+                    html_message=render_to_string('email/send_code_for_recovery_password.html', {
+                        'code': code
+                    })
+                )
+                return Response({
+                    'success': True,
+                    'message': f'E-mail enviado para {email} com sucesso!'
+                }, status=status.HTTP_200_OK)
+
+            except Exception as e:
+                return Response({
+                    'success': False,
+                    'message': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
